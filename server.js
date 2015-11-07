b5675a0e8f7e17dfc28a4d3a037e5550e3ddaaca
root <root@vps83428.ovh.net>
2015-11-07 18:37:50 +0100

    Added fingerprint detect.

--- server.js
+++ server.js
@@ -1,44 +1,41 @@
 "use strict";
 
+var getClassName = function(o) { 
+  if (o == null) {
+    return "NULL";
+  }
+  var funcNameRegex = /function (.{1,})\(/;
+  var results = (funcNameRegex).exec(o.constructor.toString());
+  return (results && results.length > 1) ? results[1] : "";
+};
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 var fs = require('fs');
 var util = require("util");
 var http2 = require("http2");
-var console = require('console');
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var options = {
   key: fs.readFileSync('/etc/letsencrypt/live/helkokbok.se/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/helkokbok.se/cert.pem'),
   requestCert: true,
-  rejectUnauthorized: false,
-  agent: false
+  rejectUnauthorized: false
 };
 
 var server = http2.createServer(options);
 
-server.on('connection', function(socket) {
-  console.log('Got connection!' + util.inspect(socket));
-});
-
-server.on('clientError', function(exception, socket) {
-  console.log('Got client error!' + util.inspect(exception));
-});
-
-server.on('error', function(exception) {
-  console.log('Got error!' + util.inspect(exception));
-});
-
 server.on('request', function(request, response) {
-  console.log('Got request!' + util.inspect(request.socket));
-  response.end(util.inspect(request.socket));
-  //if (request.socket.authorized) {
-  //  response.end('Hello client with certificate!',
-  //    + util.inspect(request.socket.getPeerCertificate()));
-  //} else {
-  //  response.end('Hello client without certificate!');
-  //}
+  var cert = request.socket.getPeerCertificate();
+  var fingerprint = null;
+  if (cert) {
+    fingerprint = cert.fingerprint;
+  }
+  if (fingerprint) {
+    response.end('Hello, your fingerprint is: ' + fingerprint);
+  } else {
+    response.end('Hello, you have no fingerprint!');
+  }
 });
 
-server.listen(8002);
+server.listen(8080);
 
