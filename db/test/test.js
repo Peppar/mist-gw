d4d9385df3e54d63835251f3a85af53cd96f16d7
Mattias Wingstedt <mattias@wingstedt.org>
2015-11-17 23:26:06 +0100

    Slight refactoring plus new tests

--- db/test/test.js
+++ db/test/test.js
@@ -1,21 +1,37 @@
 var assert = require('assert');
 var MistDb = require( '../mistDb.js' );
-var db = new MistDb.mistDb( ':memory:' );// 'test.sqlite' );
-var res = {}
+var db = new MistDb.DB( ':memory:' );// 'test.sqlite' );
+var res = {};
+var objA, objB;
 
 describe( 'MistDb', function() {
     describe( '#runTransaction()', function() {
         it( 'should create two objects', function(done) {
             var transaction = new MistDb.Transaction();
 
-            var objA = transaction.newObject( MistDb.ROOT, 'A', null, null, { a: 17, b: 'string', c: { a: 17, b: 18 } } );
-            var objB = transaction.newObject( objA, 'B', null, null, { a: 17, b: 'string', c: { a: 17, b: 18 } } );
+            objA = transaction.newObject( MistDb.ROOT, 'A', null, null, { a: 17, b: 'string', c: { a: 17, b: 18 } } );
+            objB = transaction.newObject( objA, 'B', null, null, { a: 17, b: 'string', c: { a: 17, b: 18 } } );
 //            var objC = transaction.newObject( { id: 'xxxx' }, 'B', null, null, { a: 17, b: 'string', c: { a: 17, b: 18 } } );
 
             return db.create().then( function() { return db.runTransaction( transaction ) } )
                 .then( done )
-//                .then( function() { console.log( "done?" ); done() } )
                 .catch( function(err) { done( err ) } );
+        }),
+        it( 'should not be possible to create a loop', function(done) {
+            var transaction = new MistDb.Transaction();
+
+            transaction.modifyObject( objA, objB, 'A', null, null, { a: 17, b: 'string', c: { a: 17, b: 18 } } );
+            return db.create().then( function() { return db.runTransaction( transaction ) } )
+                .then( function() { done( new Error( "Accepted transaction with a loop." ) ) } )
+                .catch( function(err) { if (err.errno == MistDb.ErrNo.INVALID_PARENT) done(); else done( err ); } );
+        }),
+        it( 'should not be possible to add an object with an invalid parent', function(done) {
+            var transaction = new MistDb.Transaction();
+
+            transaction.newObject( { id: 'xxxx' }, 'B', null, null, { a: 17, b: 'string', c: { a: 17, b: 18 } } );
+            return db.create().then( function() { return db.runTransaction( transaction ) } )
+                .then( function() { done( new Error( "Accepted transaction with a loop." ) ) } )
+                .catch( function(err) { if (err.errno == MistDb.ErrNo.INVALID_PARENT) done(); else done( err ); } );
         })
     })
 })
